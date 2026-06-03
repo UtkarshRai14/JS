1087db17097302fb2df25f39f7c2807d64b590ab
Utkarsh Rai <145283694+UtkarshRai14@users.noreply.github.com>
2026-06-03 08:37:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,26 @@
+let score = 33
+console.log(typeof score)
+
+let stringscore = String(score)
+console.log(typeof stringscore)
+
+let temp1 = ""
+let temp2 = "hello"
+
+let booltemp1 = Boolean(temp1)
+let booltemp2 = Boolean(temp2)
+
+console.log(booltemp1)
+console.log(booltemp2)
+
+let random1 = "123abc"
+
+let numberrandom1 = Number(random1)
+console.log(typeof numberrandom1)
+console.log(numberrandom1)
+
+let random2 = "123"
+
+let numberrandom2 = Number(random2)
+console.log(typeof numberrandom2)
+console.log(numberrandom2)
