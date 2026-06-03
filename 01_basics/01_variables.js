19fdf0f9297e255d3988f782365dce331c27eb2e
Utkarsh Rai <145283694+UtkarshRai14@users.noreply.github.com>
2026-06-03 07:48:13 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const uniqueId = 11
+let name = "Rudra"
+var password = "Rama"
+city = "Ayodhya"
+let state;  // value of state is - "undefined"
+
+
+// don't use var ever because 
+// it has problem of block scope and functional scope
+
+console.log(uniqueId)
+console.table([name, password, city, state])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  // tell that the code is written in new standards
+
+// alert() --> it is used in browser not in node
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value (its type is Object)
+// undefined -> 
+// symbol -> unique  
+
+// object
+
+console.log(typeof 12)
+console.log(typeof undefined)
+console.log(typeof null)
